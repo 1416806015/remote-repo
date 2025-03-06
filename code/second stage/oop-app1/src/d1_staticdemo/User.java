cd9d73818c97e1ec8fdfd163e95bdd72b80e1053
chenhongjie <1416806015@qq.com>
2025-03-06 19:08:08 +0800

    enum

---
+++ code/second stage/oop-app1/src/d1_staticdemo/User.java
@@ -0,0 +1,13 @@
+package d1_staticdemo;
+
+public class User {
+    //类变量
+    public static int number;
+
+    public User(){
+//        User.number++;
+        // 注意:在同一个类中，访问自己类的类变量，才可以省略不写
+        number++;
+    }
+
+}
