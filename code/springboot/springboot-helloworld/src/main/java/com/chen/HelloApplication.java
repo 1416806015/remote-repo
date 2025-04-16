4486323d7f84606f285d6e17f4c4d105e87b6a81
chenhongjie <1416806015@qq.com>
2025-04-16 21:31:35 +0800

    commit

---
+++ code/springboot/springboot-helloworld/src/main/java/com/chen/HelloApplication.java
@@ -0,0 +1,12 @@
+package com.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 引导类  spring boot项目的入口
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
---
+++ code/springboot/springboot-helloworld/src/test/java/com/chen/controller/springboothelloworld/SpringbootHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.controller.springboothelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
